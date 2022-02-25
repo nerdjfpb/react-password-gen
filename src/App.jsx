bee6339c26b358c1d0de153094418103fbb942b5
Muhammad Ali <nerdjfpb@gmail.com>
2022-02-26 00:06:33 +0600

    cleaning

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,8 @@ const App = () => {
       Math.random().toString(36).slice(2) + Math.random().toString(36).slice(2)
 
     // Math.random()// Generate random number, eg: 0.123456
-    //.toString(36)// Convert  to base-36 : "0.4fzyo82mvyr"
-    //.slice(2);// Cut off last 8 characters : "yo82mvyr"
+    // .toString(36)// Convert  to base-36 : "0.4fzyo82mvyr"
+    // .slice(2);// Cut off last 8 characters : "yo82mvyr"
     // Adding 2 because the password will be bigger with this
 
     // set the generated password as state
@@ -29,7 +29,6 @@ const App = () => {
     <div className="background">
       <h1 className="heading">Generate a random password</h1>
       <div className="wrapper">
-        <div className="icon"></div>
         <div className="password">{password}</div>
         <button className="generate-password" onClick={generatePassword}>
           Generate password
